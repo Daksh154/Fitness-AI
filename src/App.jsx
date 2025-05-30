6cb21148c57e6bcd9e1b028510cb092762230f78
Daksh Agarwal <agarwaldaksh154@gmail.com>
2025-05-31 01:26:02 +0530

    Updae Config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import WorkoutPlan from './pages/WorkoutPlan';
 import DietPlan from './pages/DietPlan';
@@ -12,12 +12,14 @@ function App() {
     <BrowserRouter>
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
         <Routes>
-          <Route path = "/signin" element={<LoginPage />} />
-          <Route path = "/signup" element={<SignupPage />} />
+          <Route path="/" element={<Navigate to="/signin" replace />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/home" element={<LandingPage />} />
           <Route path="/workout" element={<WorkoutPlan />} />
           <Route path="/diet" element={<DietPlan />} />
           <Route path="/progress" element={<ProgressPage />} />
+          <Route path="*" element={<Navigate to="/signin" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
